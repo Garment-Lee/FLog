3143dcbbd0670f98082a3ac6d6b9d979cb00de0a
garmentlee <garment1991@163.com>
2019-03-08 18:51:23 +0800

    add log function

--- app/src/main/java/com/ligf/logtest/MainActivity.java
+++ app/src/main/java/com/ligf/logtest/MainActivity.java
@@ -2,12 +2,59 @@ package com.ligf.logtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.ligf.flog.FLog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button mStartBtn;
+
+    private int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        init();
+    }
+
+    private void initViews(){
+        mStartBtn = findViewById(R.id.btn_start);
+        mStartBtn.setOnClickListener(this);
+    }
+
+    private void init(){
+        FLog.setSaveToFileFlag(true);
+        FLog.setShowLogFlag(true);
+        FLog.init();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true){
+                            try {
+                                Thread.sleep(3000);
+                                num ++;
+                                FLog.i("test log:" + num);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+                break;
+            default:
+
+                break;
+
+        }
     }
 }
---
+++ flog/src/main/java/com/ligf/flog/FLog.java
@@ -0,0 +1,228 @@
+package com.ligf.flog;
+
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FLog {
+
+    /**默认的Tag*/
+    private static String DEFAULT_TAG = "FLog";
+
+    private static SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+
+    /**默认最大的文件大小10M*/
+    private static int DEFAULT_MAX_FILE_SIZE = 10 * 1024 * 1024;
+
+    /**默认的文件保存目录*/
+    private static String DEFAULT_FILE_DIRECTORY = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "FLog";
+
+    /**显示Log信息的标志*/
+    private static boolean mShowLogFlag;
+
+    /**保存Log信息到文件的标志*/
+    private static boolean mSaveToFileFlag;
+
+    /**保存文件的路径*/
+    private static String mSaveFileDirectory;
+
+    /**完整的日志文件名称,包括文件完整的文件路径*/
+    private static String mAbsoluteFileName;
+
+    /**最大的日志文件大小*/
+    private static int mMaxFileSize;
+
+    public static void setShowLogFlag(boolean flag) {
+        mShowLogFlag = flag;
+    }
+
+    public static void setSaveToFileFlag(boolean flag) {
+        mSaveToFileFlag = flag;
+    }
+
+    public static void setSaveFilePath(String path) {
+        mSaveFileDirectory = path;
+    }
+
+    public static void setMaxFileSize(int fileSize){
+        mMaxFileSize = fileSize;
+    }
+
+    public static void init() {
+        if (mSaveToFileFlag) {
+            if (TextUtils.isEmpty(mSaveFileDirectory)) {
+                mSaveFileDirectory = DEFAULT_FILE_DIRECTORY;
+            }
+            mAbsoluteFileName = getLatestFileName(mSaveFileDirectory);
+        }
+        if (mMaxFileSize == 0){
+            mMaxFileSize = DEFAULT_MAX_FILE_SIZE;
+        }
+    }
+
+    public static void v(String msg) {
+        v(DEFAULT_TAG, msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (mShowLogFlag) {
+            String[] logArr = getWrappedTagMsg(tag, msg);
+            Log.v(logArr[0], logArr[1]);
+        }
+        if (mSaveToFileFlag) {
+            saveToFile(tag, msg);
+        }
+    }
+
+    public static void d(String msg) {
+        d(DEFAULT_TAG, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (mShowLogFlag) {
+            String[] logArr = getWrappedTagMsg(tag, msg);
+            Log.d(logArr[0], logArr[1]);
+        }
+        if (mSaveToFileFlag) {
+            saveToFile(tag, msg);
+        }
+    }
+
+    public static void i(String msg) {
+        i(DEFAULT_TAG, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        if (mShowLogFlag) {
+            String[] logArr = getWrappedTagMsg(tag, msg);
+            Log.i(logArr[0], logArr[1]);
+        }
+        if (mSaveToFileFlag) {
+            saveToFile(tag, msg);
+        }
+    }
+
+    public static void w(String msg) {
+        w(DEFAULT_TAG, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (mShowLogFlag) {
+            String[] logArr = getWrappedTagMsg(tag, msg);
+            Log.w(logArr[0], logArr[1]);
+        }
+        if (mSaveToFileFlag) {
+            saveToFile(tag, msg);
+        }
+    }
+
+    public static void e(String msg) {
+        e(DEFAULT_TAG, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (mShowLogFlag) {
+            String[] logArr = getWrappedTagMsg(tag, msg);
+            Log.e(logArr[0], logArr[1]);
+        }
+        if (mSaveToFileFlag) {
+            saveToFile(tag, msg);
+        }
+    }
+
+    /**
+     * 获取文件路径中的最新生成的文件名
+     *
+     * @param fileDirectory
+     * @return
+     */
+    private static String getLatestFileName(String fileDirectory) {
+        File file = new File(fileDirectory);
+        int latestFileNum = 0;
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            for (File file1 : files) {
+                String fileNum = file1.getName().substring(4);
+                if (Integer.valueOf(fileNum) > latestFileNum) {
+                    latestFileNum = Integer.valueOf(fileNum);
+                }
+            }
+        }
+        return fileDirectory + File.separator + "flog" + latestFileNum + ".txt";
+    }
+
+    private static void saveToFile(String tag, String msg) {
+        File saveFile = new File(mAbsoluteFileName);
+        String[] logArr = getWrappedTagMsg(tag, msg);
+        byte[] outputBytes = ("[" + logArr[0] + "]" + logArr[1]).getBytes();
+        try {
+            if (!saveFile.exists()) {
+                if (!saveFile.getParentFile().exists()) {
+                    saveFile.getParentFile().mkdirs();
+                }
+                saveFile.createNewFile();
+            } else if (saveFile.length() >= DEFAULT_MAX_FILE_SIZE){
+                generateNewFile(mSaveFileDirectory);
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream(saveFile, true);
+            fileOutputStream.write(mDateFormat.format(new Date()).getBytes());
+            fileOutputStream.write(outputBytes);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 生成新的Log文件
+     * @param fileDirectory
+     * @return
+     * @throws IOException
+     */
+    private static File generateNewFile(String fileDirectory) throws IOException {
+        File file = new File(fileDirectory);
+        int latestFileNum = 0;
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            for (File file1 : files) {
+                String fileNum = file1.getName().substring(4);
+                if (Integer.valueOf(fileNum) > latestFileNum) {
+                    latestFileNum = Integer.valueOf(fileNum);
+                }
+            }
+        }
+        File newFile = new File(fileDirectory + File.separator + "flog" + (latestFileNum + 1) + ".txt");
+        newFile.createNewFile();
+        return newFile;
+    }
+
+    private static String[] getWrappedTagMsg(String tag, String msg) {
+        String[] stringArr = new String[2];
+        StackTraceElement stackTraceElement = getTargetStackTraceElement();
+        stringArr[0] = TextUtils.isEmpty(tag) ? stackTraceElement.getClassName() : tag;
+        stringArr[1] = "[ (" + stackTraceElement.getClassName() + ":" + stackTraceElement.getLineNumber() + ") #" + stackTraceElement.getMethodName() + "]" + msg;
+        return stringArr;
+    }
+
+    private static StackTraceElement getTargetStackTraceElement() {
+        StackTraceElement targetStackTraceElement = null;
+        boolean shouldTrace = false;
+        StackTraceElement[] arrayElement = Thread.currentThread().getStackTrace();
+        for (int i = 0; i < arrayElement.length; i++) {
+            boolean isLogMethod = arrayElement[i].getClassName().equals(FLog.class.getName());
+            if (shouldTrace && !isLogMethod) {
+                targetStackTraceElement = arrayElement[i];
+                break;
+            }
+            shouldTrace = isLogMethod;
+        }
+        return targetStackTraceElement;
+    }
+}
